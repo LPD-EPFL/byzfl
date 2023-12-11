242d54b99c5e88b6e8165f6c4311c47729c47812
Geovani <geovani@tsf-452-wpa-5-066.epfl.ch>
2023-12-11 17:09:41 +0100

    updates but still untested

--- client.py
+++ client.py
@@ -6,25 +6,38 @@ import collections
 
 class Client(object):
     def __init__(self, 
+                 training_dataloader,
+                 test_dataloader,
                  model,
-                 momentum = None,
                  weight_decay = 1e-4, 
+                 loss,
                  lr = 0.1, 
                  lr_decay = None, 
-                 milestones = None, 
+                 momentum = None,
+                 milestones = None,
+                 labelflipping = False, 
                  device = 'cpu'):
 
+        self.training_dataloader = training_dataloader
+        self.test_dataloader = test_dataloader
 
-        self.model = getattr(models, model)().to(device).eval()
+        self.model = getattr(models, model)().to(device)
 
         self.lr = lr        
+        self.criterion = getattr(torch.nn, loss)()
         self.optimizer = torch.optim.SGD(self.model.parameters(), 
                                          lr = lr, 
-                                         weight_decay = weight_decay)
+                                         weight_decay = weight_decay, 
+                                         momentum = momentum, 
+                                         dampening = 1 - momentum)
 
         self.scheduler = torch.optim.scheduler.MuliStepLR(self.optimizer, 
                                           milestones = milestones, 
                                           gamma = lr_decay)
+
+        self.gradient_LF = 0
+        self.labelflipping = labelflipping
+
 
 
     def flatten(state_dict):
@@ -42,6 +55,27 @@ class Client(object):
             c = c + nb_element
         return new_dict
 
+
+    def compute_gradients(self):
+
+        #JS: compute gradient on flipped labels
+        if self.labelflipping:
+            self.model.eval()
+            self.model.zero_grad()
+            targets_flipped = targets.sub(self.numb_labels - 1).mul(-1)
+            outputs = sel.model(inputs)
+            loss = self.criterion(outputs, targets)
+            loss.backward()
+            self.gradient_LF = self.get_gradients()
+
+        self.model.train()
+        self.model.zero_grad()
+        inputs, targets = self.sample_train_batch()
+        inputs, targets = inputs.to(self.device), targets.to(self.device)
+        outputs = sel.model(inputs)
+        loss = self.criterion(outputs, targets)
+        loss.backward()
+
     def set_parameters(self, flat_vector):
         new_dict = unflatten_parameters(flat_vector)
         self.model.load_state_dict(new_dict)
@@ -49,7 +83,7 @@ class Client(object):
     def get_gradients(self):
         new_dict = collections.OrderedDict()
         for key, value in self.model.named_parameters():
-            new_dict[key] = value
+            new_dict[key] = value.grad
         return new_dict
 
     def get_parameters(self):
--- train.py
+++ train.py
@@ -5,8 +5,3 @@ with open('config.json') as config_file:
 
 # device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-
-train_loader_dict, test_loader = dataset.make_train_test_datasets(config)
-	
-server = 
-
