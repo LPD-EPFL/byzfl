6f7224c038b2625693c784ee8c53ca50f18b0af7
GeovaniRizk <geovani.rizk@gmail.com>
2024-11-30 20:53:06 +0100

    updated the doc

--- docs/conf.py
+++ docs/conf.py
@@ -52,7 +52,7 @@ html_static_path = ['_static']
 html_css_files = ['custom.css','https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css']
 html_show_sourcelink = False
 html_theme_options = {
-    "header_links_before_dropdown": 3,  # Adjust based on the layout
+    "header_links_before_dropdown": 5,  # Adjust based on the layout
     "icon_links": [
         {
             "name": "GitHub",
